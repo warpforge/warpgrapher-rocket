0f75817530db6cfe6815e5eb4da58a06ad0af153
maldrake <16956831+maldrake@users.noreply.github.com>
2022-01-31 19:23:31 +0000

    Update dependency versions.

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,105 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-#[macro_use] extern crate rocket;
+extern crate rocket;
 
-use juniper_rocket::GraphQLResponse;
-use rocket::State;
-use rocket::config::Environment;
-use rocket::http::{Status};
-use rocket_contrib::json::Json;
+use rocket::http::{ContentType, Status};
+use rocket::response::{content, Responder, Response};
+use rocket::serde::json::Json;
+use rocket::Request;
+use rocket::{get, post, routes, State};
+use serde::Deserialize;
+use serde_json::Value;
 use std::collections::HashMap;
-use std::process::exit;
-use warpgrapher::{Engine};
-use warpgrapher::juniper::http::GraphQLRequest;
+use std::convert::TryFrom;
+use std::io::Cursor;
+use warpgrapher::engine::config::Configuration;
+use warpgrapher::engine::context::RequestContext;
+use warpgrapher::engine::database::neo4j::Neo4jEndpoint;
+use warpgrapher::engine::database::DatabaseEndpoint;
+use warpgrapher::juniper::http::playground::playground_source;
+use warpgrapher::Engine;
 
-mod app;
+static CONFIG: &str = "
+version: 1
+model:
+  - name: User
+    props:
+      - name: name
+        type: String
+  - name: Team
+    props: 
+      - name: name
+        type: String
+";
 
-#[get("/graphiql")]
-fn graphiql() -> rocket::response::content::Html<String> {
-    juniper_rocket::playground_source("/graphql")
+#[derive(Clone, Debug)]
+struct Rctx {}
+
+impl RequestContext for Rctx {
+    type DBEndpointType = Neo4jEndpoint;
+
+    fn new() -> Self {
+        Rctx {}
+    }
+}
+
+#[derive(Clone, Debug, Deserialize)]
+struct GraphqlRequest {
+    pub query: String,
+    pub variables: Option<Value>,
+}
+
+pub struct GraphQLResponse(Status, String);
+
+impl<'r, 'o: 'r> Responder<'r, 'o> for GraphQLResponse {
+    fn respond_to(self, _: &'r Request) -> Result<Response<'o>, Status> {
+        let GraphQLResponse(status, body) = self;
+
+        Ok(Response::build()
+            .header(ContentType::new("application", "json"))
+            .status(status)
+            .sized_body(body.len(), Cursor::new(body))
+            .finalize())
+    }
+}
+
+#[get("/playground")]
+fn playground() -> rocket::response::content::Html<String> {
+    content::Html(playground_source("/graphql", None))
 }
 
 #[post("/graphql", data = "<request>")]
-fn graphql(
-    engine: State<Engine<(), ()>>,
-    request: Json<GraphQLRequest>
-) -> GraphQLResponse {
+async fn graphql(engine: &State<Engine<Rctx>>, request: Json<GraphqlRequest>) -> GraphQLResponse {
     let metadata: HashMap<String, String> = HashMap::new();
-    let resp = engine.execute(&request.into_inner(), &metadata);
-    GraphQLResponse::custom(Status::Ok, resp.unwrap())
-}
-
-fn run_rocket_server(port: u16, engine: Engine<(), ()>) {
-    println!("Running warpgrapher on http://localhost:{}/graphiql", port);
-    let config = rocket::config::Config::build(Environment::Staging)
-        .log_level(rocket::config::LoggingLevel::Off)
-        .address("0.0.0.0")
-        .port(port)
-        .finalize()
-        .unwrap();
-    rocket::custom(config)
-        .manage(engine)
-        .mount("/", routes![graphql, graphiql])
-        .attach(rocket_cors::CorsOptions::default().to_cors().unwrap())
-        .launch();
+    let resp = engine
+        .execute(
+            request.query.to_string(),
+            request.variables.clone(),
+            metadata,
+        )
+        .await;
+    GraphQLResponse(Status::Ok, resp.unwrap().to_string())
 }
 
 #[tokio::main]
 async fn main() {
-    env_logger::init();
-    let engine = match app::create_app_engine().await {
-        Ok(v) => v,
-        Err(e) => {
-            println!("[ERROR] {:#?}", e);
-            exit(1);
-        }
-    };
-    run_rocket_server(8888, engine);
+    // parse config
+    let config = Configuration::try_from(CONFIG.to_string()).expect("Expected to parse config.");
+
+    // define database endpoint
+    let db = Neo4jEndpoint::from_env()
+        .expect("Expected to read environment variables")
+        .pool()
+        .await
+        .expect("Expected to create database.");
+
+    // create warpgrapher engine
+    let engine: Engine<Rctx> = Engine::<Rctx>::new(config, db)
+        .build()
+        .expect("Expected engine build.");
+    println!("Running warpgrapher on http://localhost:8000/graphql");
+    rocket::build()
+        .manage(engine)
+        .mount("/", routes![graphql, playground])
+        .launch()
+        .await
+        .expect("Expected successfull rocket launch")
 }
